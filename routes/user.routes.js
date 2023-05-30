d872613ec84a8f202839e965c92008f6e73dc436
martina024 <107464455+martina024@users.noreply.github.com>
2023-05-31 03:49:28 +0530

    updated user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,73 +1,66 @@
 const express=require("express")
-require("dotenv").config()
-const jwt = require('jsonwebtoken');
+
+var jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const { UserModel } = require("../model/user.model");
+require("dotenv").config()
 
 
 const userRouter=express.Router()
 
 
-userRouter.post("/signup", async (req, res) => {
-    const { email, password, name, age } = req.body;
-  
-    try {
-      const userEmail = await UserModel.findOne({ email });
-  
-      if (userEmail) {
-        return res.send({ "message": "This Email is already registered" });
-      }
-  
-      bcrypt.hash(password, 5, async (err, hashedPassword) => {
-        if (err) {
-          console.log(err);
-          return res.send({ "message": "SignUp failed, please try again" });
-        }
-        else{
-            const user = new UserModel({
-                email,
-                password: hashedPassword,
-                name,
-                age
-              });
-              await user.save();
-              console.log(user);
-              res.send({ "message": "Registered Successfully" });
-        }
-  
+   
+
+userRouter.post("/signup" , async(req,res)=>{
+    const {email,password,name,age}=req.body
+   
+    try{
+        bcrypt.hash(password, 5, async(err, secure_password)=> {
+            if(err){
+                console.log(err)
+            }else{
+                    const user = new UserModel({email,password:secure_password,name,age})
+                    await user.save()
+                    console.log(user)
+                        res.send({"message" :"Registered"})
+             }
+    });
        
-      });
-    } 
-    catch (err) {
-      console.log(err);
-      res.send({ "message": "SignUp failed, please try again" });
     }
-  });
-  
+    catch(err){
+        console.log(err)
+        console.log({"message":"Something went wrong in registering"})
+    }
+})
 
 
 userRouter.post("/login" , async(req,res)=>{
     const {email,password}=req.body
     try{
 
-    const user=await UserModel.find({email})
+        
+
+        const user=await UserModel.find({
+            email
+       
+        })
        console.log(user)
        
         if(user.length>0){
             bcrypt.compare(password, user[0].password, (err, result) =>{
-               
+             
                 if(result){
                     const token=jwt.sign({userID:user[0]._id},process.env.key)
-                    res.send({"message" :"Login successuful","token":token})
+                    res.send({"message" :"login successuful","token":token})
                 }
                 else{
-                    res.send({"message" :"Wrong Credientials"})
+                    res.send({"message" :"Wrong credientials"})
                 }
             });
            
         }
         else{
-            res.send({"message" :"Wrong Credientials"})
+            res.send({"message" :"Wrong credientials"})
         }
        
         
@@ -78,6 +71,15 @@ userRouter.post("/login" , async(req,res)=>{
         res.send({"message":"Something went wrong"})
     }
 })
+
+
+
+
+
+
+
+
+
 
 
 
